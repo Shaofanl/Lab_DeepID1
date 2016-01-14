b0d0ca9fc4e178ca33e82458cf99d3edb86bd71e
piscesdream <hapiscesdream@gmail.com>
2016-01-14 13:49:21 +0000

    update

--- deepid.py
+++ deepid.py
@@ -25,8 +25,8 @@ def getMulticlassNet(lmdb, batch_size):
 
     n.conv4 = L.Convolution(n.pool3, kernel_size=2, stride=1, num_output=80, weight_filler=dict(type='xavier'))
 
-    n.conv4deepid = L.InnerProduct(n.conv4, num_output=320, weight_filler=dict(type='xavier'))
-    n.pool3deepid = L.InnerProduct(n.pool3, num_output=320, weight_filler=dict(type='xavier'))
+    n.conv4deepid = L.InnerProduct(n.conv4, num_output=160, weight_filler=dict(type='xavier'))
+    n.pool3deepid = L.InnerProduct(n.pool3, num_output=160, weight_filler=dict(type='xavier'))
 
     n.deepid__ = L.Eltwise(n.conv4deepid, n.pool3deepid, eltwise_param={'operation': 1})
     n.deepid = L.ReLU(n.deepid__)
@@ -42,15 +42,14 @@ def createSolver(filename, **kwargs):
             f.write('{}: {}\n'.format(key, val))
     return filename
 
-def runSolver(solver, predict_layer, test_set=False, N=10):
+def runSolver(solver, predict_layer, test_set=False, rounds=10, epochs=100):
     print [(k, v.data.shape) for k, v in solver.net.blobs.items()]
     print [(k, v[0].data.shape) for k, v in solver.net.params.items()]
 
-    test_interval = 100
     # losses will also be stored in the log
 
     # the main solver loop
-    for it in range(N):
+    for it in range(rounds):
         # accuracy
         correct = 0
         for i in xrange(10):
@@ -58,7 +57,7 @@ def runSolver(solver, predict_layer, test_set=False, N=10):
             correct += (solver.net.blobs[predict_layer].data.argmax(1) == solver.net.blobs['label'].data).sum()
         print 'Train: [%04d]: %04d/1000=%.5f%%' % (it, correct, correct/1e3*1e2)
 
-        solver.step(test_interval)  # SGD by Caffe
+        solver.step(epochs)  # SGD by Caffe
 
 if __name__ == '__main__':
     caffe.set_mode_gpu()
@@ -69,8 +68,9 @@ if __name__ == '__main__':
     multiclass_solver = caffe.SGDSolver(
         createSolver('multiclassSolver.prototxt', 
             train_net='"./lfw_multiclass.prototxt"',
-            base_lr=0.010,
-            momentum=0.9,
+            base_lr=5e-2,
+#            rms_decay=0.02,
+#            momentum=0.9,
             weight_decay=0.001,
             lr_policy='"inv"',
             gamma=0.001,
@@ -80,6 +80,6 @@ if __name__ == '__main__':
             snapshot=100,
             snapshot_prefix='"/home/share/shaofan/lfw_caffe/snapshot/"',
             ))
-    runSolver(multiclass_solver, 'fc', N=20)
+    runSolver(multiclass_solver, 'fc', rounds=100, epochs=100)
 
 
--- validate.py
+++ validate.py
@@ -47,11 +47,12 @@ def get_2partdata(filename, transformer):
     return np.array(x), np.array(y)
 
 def get_feature(net, x):
-    print 'Getting feature of data whose shape is {}'.format(x.shape)
+    print '[get_feature({})]: shape={}'.format(id(x), x.shape)
     # deepid
     net.blobs['data'].reshape(*((x.shape[0], ) + net.blobs['data'].data.shape[1:]))
     net.blobs['data'].data[...] = x
     net.forward()
+    print '[get_feature({})]: feature_shape={}'.format(id(x), net.blobs['deepid'].data.shape)
     return net.blobs['deepid'].data
 
 class Threshold(object):
@@ -82,6 +83,7 @@ class Threshold(object):
         
 
 def test(clf, trainx, trainy, testx, testy, **kwargs):
+    clf.fit(trainx, trainy)
     acc = (clf.predict(trainx, **kwargs) == trainy).sum()
     print 'Train: {}/{}'.format(acc, trainy.shape[0])
 
@@ -89,10 +91,10 @@ def test(clf, trainx, trainy, testx, testy, **kwargs):
     print 'Test: {}/{}'.format(acc, testy.shape[0])
 
 
-
-
 if __name__ == '__main__':
-    net = caffe.Net('./lfw_multiclass.deploy.prototxt', '/home/share/shaofan/lfw_caffe/snapshot/_iter_800.caffemodel', caffe.TEST)
+    net = caffe.Net('./lfw_multiclass.deploy.prototxt', 
+                    '/home/share/shaofan/lfw_caffe/snapshot/_iter_2100.caffemodel', 
+                    caffe.TEST)
 # input preprocessing: 'data' is the name of the input blob == net.inputs[0]
     transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
     transformer.set_transpose('data', (2,0,1))
@@ -113,38 +115,53 @@ if __name__ == '__main__':
     trainx, trainy = get_2partdata(os.path.join(LFW_ROOT, 'pairsDevTrain.txt'), transformer)
     testx, testy= get_2partdata(os.path.join(LFW_ROOT, 'pairsDevTest.txt'), transformer)
     
+    print 'fetching features of trainx'
     trainx = get_feature(net, trainx).reshape(trainy.shape[0], 2, -1) 
     testx = get_feature(net, testx).reshape(testy.shape[0], 2, -1) 
+    print 'feature shape:', trainx.shape
     print trainx.shape
     print testx.shape
 
-    methods = ['diff', 'cosdiff']
+    methods = ['diff', 'cosdiff', 'svm']
     
+    from sklearn import svm
+    from sklearn.preprocessing import normalize
     # svm
     if 'svm' in methods:
-        __trainx = trainx[:, 0, :] - trainx[:, 1, :]
+        print 'Concatenate + SVM'
+        __trainx = np.concatenate([trainx[:, 0, :], trainx[:, 1, :]], 1)
+        __testx = np.concatenate([testx[:, 0, :], testx[:, 1, :]], 1)
+        clf = svm.SVC(kernel='linear', probability=True, verbose=False, max_iter=100000)
+        test(clf, __trainx, trainy, __testx, testy)
+
+    if 'svm' in methods:
+        print 'Minues + SVM'
+        __trainx = normalize(trainx[:, 0, :] - trainx[:, 1, :])
         __testx = testx[:, 0, :] - testx[:, 1, :]
-        from sklearn import svm
-        clf = svm.SVC(kernel='linear', probability=True, verbose=True, max_iter=3000)
-        clf.fit(__trainx, trainy)
+        clf = svm.SVC(kernel='linear', probability=True, verbose=False, max_iter=100000)
+        test(clf, __trainx, trainy, __testx, testy)
+
+        print 'Minues + SVM(rbf)'
+        clf = svm.SVC(kernel='rbf', probability=True, verbose=False, max_iter=100000)
         test(clf, __trainx, trainy, __testx, testy)
 
     # diff threshold
     if 'diff' in methods:
+        print 'Minues + L2 norm + Threshold: '
         __trainx = ((trainx[:, 0, :] - trainx[:, 1, :])**2).sum(1)
         __testx = ((testx[:, 0, :] - testx[:, 1, :])**2).sum(1)
 
         clf = Threshold()
-        clf.fit(__trainx, trainy)
         test(clf, __trainx, trainy, __testx, testy)
 
     # cosdiff threshold
     if 'cosdiff' in methods:
+        print 'Cos difference + Threshold: '
         __trainx = (trainx[:, 0, :] * trainx[:, 1, :]).sum(1)/np.sqrt((trainx[:, 0, :]**2).sum(1))/np.sqrt((trainx[:, 1, :]**2).sum(1))
         __testx = (testx[:, 0, :] * testx[:, 1, :]).sum(1)/np.sqrt((testx[:, 0, :]**2).sum(1))/np.sqrt((testx[:, 1, :]**2).sum(1))
 
         clf = Threshold()
-        clf.fit(__trainx, trainy)
         test(clf, __trainx, trainy, __testx, testy)
+
 
 
